6a3e0c1e4ad4bbdb40b2e39bc981ccb8a8210060
Rebecca Carney <rmdcarney@gmail.com>
2021-07-08 22:18:46 -0700

    Changed test script.

--- 0-trafficLight-checkConnections.ino
+++ 0-trafficLight-checkConnections.ino
@@ -8,33 +8,58 @@
 * 
 * ==========================
 * This script will be used to test that your circuit is working correctly.
+* Adapted from: Love to Code, Volume 1: Basic Blink
 */
 
 
 //----------------------
 // Initialize variables
 //----------------------
-int redLED = 1;   // initialize red LED as pin 1        
-int amberLED = 3; // initialize amber LED as pin 3        
-int greenLED = 5; // initialize green LED as pin 5        
-
+int LED0 = 0;   // initialize LED0 as pin 0        
+int LED1 = 1;   // initialize LED1 as pin 1        
+int LED2 = 2;   // initialize LED2 as pin 2        
+int LED3 = 3;   // initialize LED3 as pin 3        
+int LED4 = 4;   // initialize LED4 as pin 4        
+int LED5 = 5;   // initialize LED5 as pin 5        
+int pauseInterval = 200; //Pause interval in milliseconds (ms)
 
 //----------------------
 // Setup pins on chip
 //----------------------
+// set all pins as outputs
 void setup() {
-  outputMode(redLED);      // set red LED pin as output
-  outputMode(amberLED);    // set amber LED pin as output
-  outputMode(greenLED);    // set green LED pin as output
+  outputMode(LED0);      
+  outputMode(LED1); 
+  outputMode(LED2); 
+  outputMode(LED3); 
+  outputMode(LED4); 
+  outputMode(LED5); 
 }
 
 //----------------------
 // L O O P
 //----------------------
 // NB: Everything in the loop is repeated
+// Turn on the LEDs across all 6 pins in sequence. 
 void loop() {
 
-    on(redLED);
-    on(amberLED);
-    on(greenLED);
+    on(LED0);
+    pause(pauseInterval);
+    off(LED0);
+  	on(LED1);
+    pause(pauseInterval);
+  	off(LED1);
+  	on(LED2);
+    pause(pauseInterval);
+    off(LED2);
+  	on(LED3);
+    pause(pauseInterval);
+	off(LED3);
+  	on(LED4);
+    pause(pauseInterval);
+	off(LED4);
+  	on(LED5);
+	pause(pauseInterval);
+	off(LED5);
 }
+
